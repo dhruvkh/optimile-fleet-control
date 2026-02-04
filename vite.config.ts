7aba7b8ba460dfa174b475c7d8f154fd01289897
vickytsquaredc <154512538+vickytsquaredc@users.noreply.github.com>
2026-02-04 12:14:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/fleet/",
+      // base: "/fleet/",
       server: {
         port: 4000,
         host: '0.0.0.0',
